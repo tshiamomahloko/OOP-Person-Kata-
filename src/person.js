e5f3b1375bcacbc111a25c34328c58ff0bb0e954
tshiamomahloko <58834090+tshiamomahloko@users.noreply.github.com>
2020-05-25 15:53:28 +0200

    Update person.js

--- src/person.js
+++ src/person.js
@@ -6,8 +6,13 @@ class Person {
         this.interests = interests
     }
     hello() {
-        return (`Hello, my name is ${this.name} and I am ${this.age} years old ${this.gender}. My interests are ${this.interests}.`)
+        return (`Hello, my name is ${this.name} and I am ${this.age} years old ${this.gender}. My interests are ${this.interests[0]}, ${this.interests[1]} and ${this.interests[2]} .`)
     }
 }
 
+let person = new Person('Ryan',30,'male',['being a hardarse','agile', 'ssd hard drives'] )
+
+let greeting = person.hello()
+
+console.log(greeting)
 module.exports = {Person: Person}
